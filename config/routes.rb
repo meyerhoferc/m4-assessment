576cb80567e2e04e12a4992c72cd74e31962ac70
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-05-22 10:11:38 -0600

    Add feature test for creating user account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "links#index"
 
+  get 'authenticate', to: 'authenticate#index'
+
   resources :links, only: [:index]
 
   namespace :api do
---
+++ spec/features/users/user_can_create_account_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+# As an unauthenticated user, when I visit /,
+# I should be redirected to a page which prompts me to "Log In or Sign Up".
+# As an unauthenticated user, if I click "Sign Up", I should be taken to a form where I can enter an email address, a password, and a password confirmation.
+#
+#     I cannot sign up with an email address that has already been used.
+#     I cannot sign up without an email address and a password.
+#     Password and confirmation must match.
+#     If criteria is not met the user should be given a message to reflect the reason they could not sign up.
+#
+# Upon submitting this information, I should be logged in and redirected to the "Links Index" page.
+describe "user account creation" do
+  it "requires an email and password" do
+    visit root_path
+    expect(current_path).to eq(authenticate_path)
+    click_on 'Sign Up'
+    expect(current_path).to eq(new_user_path)
+
+    fill_in('user[email]', with: 'email@email.com')
+    fill_in('user[password]', with: 'password')
+    fill_in('user[password__confirmation]', with: 'password')
+
+    click_on 'Sign Up'
+
+    expect(current_path).to eq(root_path)
+  end
+end
