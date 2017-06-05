727dc8db20673a98910bc7394cf457bd0d162af7
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-06-04 20:35:06 -0600

    Add request test for PUT /api/v1/links/:id

--- spec/features/links/user_can_mark_link_as_read_spec.rb
+++ spec/features/links/user_can_mark_link_as_read_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-describe 'user can mark link as read' do
+describe 'user can mark link as read', :js => :true do
   it 'for an unread link' do
     current_user = User.create!(email: 'example@email.com', password: 'password')
     link = current_user.links.create!(title: 'my first link', url: 'https://news.ycombinator.com/', read: false)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
     visit root_path
-    save_and_open_page
+    # save_and_open_page
     within('.links') do
       expect(page).to have_content('Unread')
       expect(page).to have_button('Mark as Read')
     end
+
+    expect(link.read).to eq(false)
 
     within('.links') do
       click_on 'Mark as Read'
@@ -20,5 +22,7 @@ describe 'user can mark link as read' do
       expect(page).to have_content('Read')
       expect(page).to have_link('Mark as Unread')
     end
+
+    expect(link.read).to eq(true)
   end
 end
