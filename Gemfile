c5e6a3a976f24ae0476a0b2966a543023b89f5e2
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-06-05 19:37:20 -0600

    Add HotReads service and test

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'responders'
 gem 'awesome_print'
 gem 'sprockets-es6'
 gem 'bcrypt', '~> 3.1.7'
+gem 'httparty'
 
 # gem 'unicorn'
 
@@ -36,4 +37,6 @@ group :development, :test do
   gem 'factory_girl_rails'
   gem 'shoulda-matchers'
   gem 'selenium-webdriver', '~> 2.53.4'
+  gem 'vcr'
+  gem 'webmock'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,14 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+end
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(
